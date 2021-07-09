63a920282ece566ebc841e1fbf69ca87e9d0d0f8
aliIsazadeh <isazadhali@gmail.com>
2021-07-09 14:44:32 +0430

    jwt

---
+++ src/main/java/com/example/demo/JWT/JwtConfig.java
@@ -0,0 +1,47 @@
+package com.example.demo.JWT;
+
+import com.google.common.net.HttpHeaders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+@Component
+@ConfigurationProperties(prefix = "application.jwt")
+public class JwtConfig {
+    private String secretKey;
+    private String tokenPrefix;
+    private String tokenExpirationAfterDays;
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    public String getTokenPrefix() {
+        return tokenPrefix;
+    }
+
+    public void setTokenPrefix(String tokenPrefix) {
+        this.tokenPrefix = tokenPrefix;
+    }
+
+    public String getTokenExpirationAfterDays() {
+        return tokenExpirationAfterDays;
+    }
+
+    public void setTokenExpirationAfterDays(String tokenExpirationAfterDays) {
+        this.tokenExpirationAfterDays = tokenExpirationAfterDays;
+    }
+
+
+    public String getAuthorizationHeader(){
+        return HttpHeaders.AUTHORIZATION;
+    }
+}
