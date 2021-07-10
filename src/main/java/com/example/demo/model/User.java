d170913965e61fd2cd4bfb9cb48bc1751536f20e
aliIsazadeh <isazadhali@gmail.com>
2021-07-10 13:19:09 +0430

    user

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.Set;
 
 @Data
@@ -15,82 +16,91 @@ import java.util.Set;
 
 @NoArgsConstructor
 
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @Transient
-    private Set<? extends GrantedAuthority> grantedAuthorities;
-    private String firstName;
-    private String password;
-    private String lastName;
-    private String userName;
-    private String phoneNum;
 
+
+
+    @Transient
+    private  Set<? extends GrantedAuthority> grantedAuthorities;
+    private  String firstName;
+    private  String password;
+    private  String lastName;
+    private  String userName;
+    private  String phoneNum;
+    private  boolean isAccountNonExpired;
+    private  boolean isAccountNonLocked;
+    private  boolean isCredentialsNonExpired;
+    private  boolean isEnabled;
+
+    public User(String userName,String password,Set<? extends GrantedAuthority> grantedAuthorities, String firstName,  String lastName,  String phoneNum, boolean isAccountNonExpired, boolean isAccountNonLocked, boolean isCredentialsNonExpired, boolean isEnabled) {
+        this.grantedAuthorities = grantedAuthorities;
+        this.firstName = firstName;
+        this.password = password;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.phoneNum = phoneNum;
+        this.isAccountNonExpired = isAccountNonExpired;
+        this.isAccountNonLocked = isAccountNonLocked;
+        this.isCredentialsNonExpired = isCredentialsNonExpired;
+        this.isEnabled = isEnabled;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
 
     public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public Set<? extends GrantedAuthority> getGrantedAuthorities() {
         return grantedAuthorities;
     }
 
-    public void setGrantedAuthorities(Set<? extends GrantedAuthority> grantedAuthorities) {
-        this.grantedAuthorities = grantedAuthorities;
-    }
-
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public String getPassword() {
+        return password;
     }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     public String getUserName() {
         return userName;
-    }
-
-    public void setUserName(String username) {
-        this.userName = userName;
     }
 
     public String getPhoneNum() {
         return phoneNum;
     }
 
-    public void setPhoneNum(String phoneNum) {
-        this.phoneNum = phoneNum;
+    public boolean isAccountNonExpired() {
+        return isAccountNonExpired;
     }
 
-
-
-    public User(String userName,String password, Set<? extends GrantedAuthority> grantedAuthorities, String firstName, String lastName, String phoneNum) {
-        this.userName = userName;
-        this.password = password;
-        this.grantedAuthorities = grantedAuthorities;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.phoneNum = phoneNum;
-
+    public boolean isAccountNonLocked() {
+        return isAccountNonLocked;
     }
 
+    public boolean isCredentialsNonExpired() {
+        return isCredentialsNonExpired;
+    }
 
-    public String getName(){
-        return getFirstName()+" "+getLastName();
+    public boolean isEnabled() {
+        return isEnabled;
     }
 }
