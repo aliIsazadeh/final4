07f8cd93179aaebec682b5bb59ed8156cd048308
aliIsazadeh <isazadhali@gmail.com>
2021-07-11 14:53:19 +0430

    Announcement endpoint

---
+++ src/main/java/com/example/demo/services/AnnouncementService.java
@@ -0,0 +1,36 @@
+package com.example.demo.services;
+
+import com.example.demo.model.Announcement;
+import com.example.demo.model.User;
+import com.example.demo.repositories.AnnouncementRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnnouncementService {
+    @Autowired
+    private AnnouncementRepo announcementRepo;
+
+    public Announcement getAnnouncementById(int id){
+        return  announcementRepo.findById(id).orElseThrow(()
+                -> new UsernameNotFoundException(String.format("User with Username: %s notfound!", String.valueOf(id))));
+    }
+
+    public Announcement deleteAnnouncementById(int id){
+        announcementRepo.deleteById(id);
+        return  announcementRepo.findById(id).orElseThrow(()
+                -> new UsernameNotFoundException(String.format("User with Username: %s notfound!", String.valueOf(id))));
+    }
+
+
+    public Announcement addAnnouncement(int TT , String message){
+        Announcement announcement = new Announcement(TT,message);
+        announcementRepo.saveAndFlush(announcement);
+        return announcement;
+    }
+
+    public Announcement getAnnouncement(int masterId, int timeTableId, int pageSize, int page){
+        return  null;
+    }
+}
